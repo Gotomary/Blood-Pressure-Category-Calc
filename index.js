7cb09a871746b84cde7a222b907f0c7eb9a25197
webdevterri <32005382+webdevterri@users.noreply.github.com>
2022-04-18 00:32:14 -0500

    Changed innerText to innerHtml to add p & classes

--- index.js
+++ index.js
@@ -8,26 +8,39 @@ function calculateBP() {
   if (stolic < 120 && diastolic < 80) {
     bpCategory.style.backgroundColor = "#169D00";
     bpCategory.style.color = "#FFFFFF";
-    bpCategory.innerText = "Your blood pressure is NORMAL.";
+    bpCategory.innerHtml = (
+      <p class="output-text text-light">Your blood pressure is NORMAL.</p>
+    );
   } else if (stolic < 129 && diastolic < 80) {
     bpCategory.style.backgroundColor = "#00CF91";
     bpCategory.style.color = "#000000";
-    bpCategory.innerText = "Your blood pressure is ELEVATED.";
+    bpCategory.innerHtml = (
+      <p class="output-text text-dark">Your blood pressure is ELEVATED.</p>
+    );
   } else if (stolic < 139 || diastolic < 89) {
     bpCategory.style.backgroundColor = "#DBD200";
     bpCategory.style.color = "#000000";
-    bpCategory.innerText =
-      "You have HIGH BLOOD PRESSURE (Hypertension Stage 1)!";
+    bpCategory.innerHtml = (
+      <p class="output-text text-dark">
+        You have HIGH BLOOD PRESSURE (Hypertension Stage 1)!
+      </p>
+    );
   } else if (stolic >= 140 || diastolic >= 90) {
     bpCategory.style.backgroundColor = "#BC7100";
     bpCategory.style.color = "#FFFFFF";
-    bpCategory.innerText =
-      "You have HIGH BLOOD PRESSURE (Hypertension Stage 2)!";
+    bpCategory.innerHtml = (
+      <p class="output-text text-light">
+        You have HIGH BLOOD PRESSURE (Hypertension Stage 2)!
+      </p>
+    );
   } else if (stolic > 180 || diastolic > 120) {
     bpCategory.style.backgroundColor = "#BC0000";
     bpCategory.style.color = "#FFFFFF";
-    bpCategory.innerText =
-      "You are in HYPERTENSIVE CRISIS. SEEK EMERGENCY CARE NOW!";
+    bpCategory.innerHtml = (
+      <p class="output-text text-light">
+        You are in HYPERTENSIVE CRISIS. SEEK EMERGENCY CARE NOW!
+      </p>
+    );
   }
 }
 
